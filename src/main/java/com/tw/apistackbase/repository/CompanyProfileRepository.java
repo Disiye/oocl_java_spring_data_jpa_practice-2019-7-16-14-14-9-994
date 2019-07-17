4f79fca7a1d979f95aa8976d18f2dbe4eea22bdf
WENGEM <emilio.wang@oocl.com>
2019-07-17 19:24:00 +0800

    uodate

--- src/main/java/com/tw/apistackbase/repository/CompanyProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tw.apistackbase.repository;
-
-import com.tw.apistackbase.core.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyProfileRepository extends JpaRepository<Profile, Long> {
-}
